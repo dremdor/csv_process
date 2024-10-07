e701d528f909ccb8e5011ee474eec19b789f9df5
Dima <dima220997999@gmail.com>
2024-10-08 00:36:57 +0300

    noretrun funcs

--- main.py
+++ main.py
@@ -4,21 +4,18 @@ from datetime import datetime, date
 from typing import Tuple, Union, List
 
 
-def fill_spaces(data: List[List[str]]) -> List[List[str]]:
-    new_data = data.copy()
-
+def fill_spaces(data: List[List[str]]) -> None:
     for i in range(2, len(data)):
-        prev_temp, prev_util = float(new_data[i - 1][1]), float(new_data[i - 1][2])
-        if new_data[i][1] == "":
-            new_data[i][1] = prev_temp
-        if new_data[i][2] == "":
-            new_data[i][2] = prev_util
+        prev_temp, prev_util = float(data[i - 1][1]), float(data[i - 1][2])
 
-    return new_data
+        if data[i][1] == "":
+            data[i][1] = prev_temp
+
+        if data[i][2] == "":
+            data[i][2] = prev_util
 
 
 def count_stats(data: List[List[str]], param: int) -> Tuple[float, float, float]:
-
     min_value = max_value = float(data[1][param])
     avg = 0
 
@@ -36,11 +33,9 @@ def count_stats(data: List[List[str]], param: int) -> Tuple[float, float, float]
     return min_value, max_value, avg
 
 
-def add_columns(data: List[List[str]]) -> List[List[str]]:
-    new_data = data.copy()
-
-    min_temp, max_temp, avg_temp = count_stats(new_data, 1)
-    min_util, max_util, avg_util = count_stats(new_data, 2)
+def add_columns(data: List[List[str]]) -> None:
+    min_temp, max_temp, avg_temp = count_stats(data, 1)
+    min_util, max_util, avg_util = count_stats(data, 2)
 
     stats_headers = [
         "min_temp",
@@ -59,7 +54,7 @@ def add_columns(data: List[List[str]]) -> List[List[str]]:
     for header in stats_headers:
         headers.append(header)
 
-    for line in new_data[1:]:
+    for line in data[1:]:
         for stats in stats_data:
             line.append(stats)
 
@@ -68,24 +63,18 @@ def add_columns(data: List[List[str]]) -> List[List[str]]:
         else:
             line.append("OK")
 
-    return new_data
 
+def time_sort(data: List[List[str]]) -> None:
+    headers = ["index"] + data[0][1:]
 
-def time_sort(data: List[List[str]]) -> List[List[str]]:
-    new_data = data.copy()
-
-    headers = data[0]
-    sorted_data = sorted(
-        new_data[1:],
+    data[1:].sort(
         key=lambda x: datetime.strptime(x[3][:19], "%Y-%m-%d %H:%M:%S").timestamp(),
     )
 
-    for i, line in enumerate(sorted_data):
+    for i, line in enumerate(data):
         line[0] = str(i)
 
-    sorted_data.insert(0, headers)
-
-    return sorted_data
+    data[0] = headers
 
 
 def read_csv(file_path: str) -> List[List[str]]:
@@ -120,14 +109,24 @@ def write_json(data: List[List[str]]) -> None:
 
 def main(file_path: str) -> None:
     data = read_csv(file_path)
-    sorted_data = time_sort(data)
+    time_sort(data)
 
-    filled_data = fill_spaces(sorted_data)
-    updated_data = add_columns(filled_data)
+    fill_spaces(data)
+    add_columns(data)
 
-    write_csv(updated_data)
-    write_json(updated_data)
+    write_csv(data)
+    write_json(data)
 
 
 if __name__ == "__main__":
     main("test_data.csv")
+    # main('zero.csv')
+    # main('headers.csv')
+    # main('first_blank.csv')
+
+    # data1 = [
+    #    ['temperature', 'utilization', 'timestamp']
+    #    ['0', '59.64', '37.0', '2024-09-27 07:08:32.306912899']
+    #    ['1', '59.18', '23.0', '2024-09-27 07:08:37.306912899']
+    #    ['2', '', '', '2024-09-27 07:08:42.306912899']
+    # ]
